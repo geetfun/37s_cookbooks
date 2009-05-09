18143354d02a0a84090a0fea0da8fec02f7ce4c5
Joshua Sierles <joshua@rasgueado.local>
2009-05-09 15:32:26 +0200

    configure mysql

--- cookbooks/base/recipes/app.rb
+++ cookbooks/base/recipes/app.rb
@@ -1,5 +1,9 @@
 require_recipe "syslog::client"
 require_recipe "nagios::client"
 require_recipe "dns::client"
-
 require_recipe "rails::apps"
+
+package "mysql-client"
+package "libmysqlclient15-dev"
+gem_package "mysql"
+package "mysql-server"
