d6cbb594f7f3b665737039cd1de78afc6fe545ff
Joshua Sierles <joshua@250.Red-88-29-164.staticIP.rima-tde.net>
2009-05-09 03:08:10 +0200

    move some recipes around for noc role

--- cookbooks/base/recipes/app.rb
+++ cookbooks/base/recipes/app.rb
@@ -1,4 +1,5 @@
 require_recipe "syslog::client"
 require_recipe "nagios::client"
+require_recipe "dns::client"
 
 require_recipe "rails::apps"
--- cookbooks/base/recipes/noc.rb
+++ cookbooks/base/recipes/noc.rb
@@ -1,3 +1,4 @@
+require_recipe "dns::server"
 require_recipe "syslog::server"
 require_recipe "chef::server"
 require_recipe "nagios::server"
