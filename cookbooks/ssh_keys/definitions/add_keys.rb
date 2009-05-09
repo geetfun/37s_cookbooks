d22311d742d316f5acc1759ed5da43bb1d4e642f
Joshua Sierles <joshua@rasgueado.local>
2009-05-09 14:59:50 +0200

    update ssh keys

--- cookbooks/ssh_keys/definitions/add_keys.rb
+++ cookbooks/ssh_keys/definitions/add_keys.rb
@@ -4,11 +4,24 @@ define :add_keys do
   name = params[:name]
   keys = Mash.new
   keys[name] = node[:ssh_keys][name]
-    
+
+  if config[:ssh_key_groups]
+    config[:ssh_key_groups].each do |group|
+      node[:users].find_all{|u| u.last[:group] == group}.each do |user|
+        keys[user.first] = node[:ssh_keys][user.first]
+      end
+    end
+  end
+  
+  if node[:extra_ssh_keys]
+    node[:extra_ssh_keys].each do |username|
+      keys[username] = node[:ssh_keys][username]
+    end
+  end
+  
   template "/home/#{name}/.ssh/authorized_keys" do
     source "authorized_keys.erb"
     action :create
-    cookbook "ssh_keys"
     owner name
     group config[:group].to_s
     variables(:keys => keys)
